148c7e948285ac17f19cc698966b9c42ea0c7cb0
Sebastian Karasiewicz <sekarasiewicz@gmail.com>
2019-09-01 00:05:40 +0200

    enums

--- DotNetTutorial/Program.cs
+++ DotNetTutorial/Program.cs
@@ -4,6 +4,12 @@ namespace DotNetTutorial
 {
     class Program
     {
+        public enum ShippingMethod
+        {
+            RegularAirMail = 1,
+            RegistedAirMail = 2,
+            Express = 3
+        }
         static void Main(string[] args)
         {
             var calculate = new Calculation();
@@ -13,6 +19,18 @@ namespace DotNetTutorial
 
             var names = new string[3]{ "Setefan", "Józek", "Kazik" };
             Console.WriteLine(string.Join(", ", names));
+
+            var method = ShippingMethod.Express;
+            Console.WriteLine("Int: " + (int)method);
+
+            var methodId = 3;
+            Console.WriteLine((ShippingMethod)methodId);
+
+            Console.WriteLine(method.ToString());
+
+            var methodName = "Express";
+            Console.WriteLine("methodname - " + (ShippingMethod)Enum.Parse(typeof(ShippingMethod), methodName));
+            Console.WriteLine("methodname int - " + (int)Enum.Parse(typeof(ShippingMethod), methodName));
         }
     }
 }
