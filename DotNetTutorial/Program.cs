1b56c017981b0f81bd01e1b35fd3566d22f60b70
Sebastian Karasiewicz <sekarasiewicz@gmail.com>
2019-09-01 22:50:08 +0200

    foreach

--- DotNetTutorial/Program.cs
+++ DotNetTutorial/Program.cs
@@ -6,6 +6,7 @@ namespace DotNetTutorial
     {
         static void Main(string[] args)
         {
+            Console.Write("What is the speed limit? ");
             var cos = Console.ReadLine();
 
             Console.WriteLine(cos);
@@ -27,6 +28,34 @@ namespace DotNetTutorial
             float price = true ? 10f : 20f;
             Console.WriteLine(price);
             Console.WriteLine(string.Format("Array1[0]: {0}, array2[0]: {1}", array1[0], array2[0]));
+
+            for (var i=1; i<=10; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine("i: {0}", i);
+                }
+            }
+
+            for (var i = 10; i >= 1; i--)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine("i: {0}", i);
+                }
+            }
+
+            var name = "John Smith";
+
+            for (var i=0; i < name.Length; i++)
+            {
+                Console.WriteLine(name[i]);
+            }
+
+            foreach(var character in name)
+            {
+                Console.WriteLine(character);
+            }
         }
     }
 }
