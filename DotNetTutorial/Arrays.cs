9213794352f2e2b1bef6ea5fdad04f29716fb8ef
Sebastian Karasiewicz <sekarasiewicz@gmail.com>
2019-09-09 22:57:48 +0200

    Arrays, extract sofar to new class

---
+++ DotNetTutorial/Arrays.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace DotNetTutorial
+{
+    class Arrays
+    {
+        public void call()
+        {
+            var numbers = new [] { 3, 7, 9, 2, 14, 6 };
+            Console.WriteLine("Lengt: " + numbers.Length);
+
+            var index = Array.IndexOf(numbers, 9);
+            Console.WriteLine("Index of 9: " + index);
+
+            Array.Clear(numbers, 0, 2);
+
+            foreach (var n in numbers)
+            {
+                Console.WriteLine(n);
+            }
+
+            var another = new int[3];
+            Console.WriteLine("Coppy");
+            Array.Copy(numbers, another, 3);
+            foreach (var n in another)
+            {
+                Console.WriteLine("Another: " + n);
+            }
+
+            Console.WriteLine("Effect of sorting");
+            Array.Sort(numbers);
+            foreach (var n in numbers)
+            {
+                Console.WriteLine("Sorted: " + n);
+            }
+
+            Console.WriteLine("Reverse");
+            Array.Reverse(numbers);
+            foreach (var n in numbers)
+            {
+                Console.WriteLine("Revers: " + n);
+            }
+        }
+    }
+}
--- DotNetTutorial/Program.cs
+++ DotNetTutorial/Program.cs
@@ -6,82 +6,11 @@ namespace DotNetTutorial
     {
         static void Main(string[] args)
         {
-            Console.Write("What is the speed limit? ");
-            //var cos = Console.ReadLine();
+            var soFar = new SoFar();
+            //soFar.call();
 
-            //Console.WriteLine(cos);
-            var calculate = new Calculation();
-
-            Console.WriteLine(calculate.Add(100, 100));
-
-            var a = 10;
-            var b = a;
-
-            a++;
-
-            Console.WriteLine(string.Format("a: {0}, b: {1}", a, b));
-
-            var array1 = new int[3] { 1, 2, 3 };
-            var array2 = array1;
-
-            array2[0] = 0;
-            float price = true ? 10f : 20f;
-            Console.WriteLine(price);
-            Console.WriteLine(string.Format("Array1[0]: {0}, array2[0]: {1}", array1[0], array2[0]));
-
-            for (var i=1; i<=10; i++)
-            {
-                if (i % 2 == 0)
-                {
-                    Console.WriteLine("i: {0}", i);
-                }
-            }
-
-            for (var i = 10; i >= 1; i--)
-            {
-                if (i % 2 == 0)
-                {
-                    Console.WriteLine("i: {0}", i);
-                }
-            }
-
-            var name = "John Smith";
-
-            for (var i=0; i < name.Length; i++)
-            {
-                Console.WriteLine(name[i]);
-            }
-
-            foreach(var character in name)
-            {
-                Console.WriteLine(character);
-            }
-
-            Console.WriteLine(string.IsNullOrWhiteSpace(" "));
-
-            var random = new Random();
-            const int passwordLength = 10;
-
-
-            for (var i = 0; i < passwordLength; i++)
-            {
-                Console.WriteLine(random.Next(1, 10));
-            }
-
-            for (var i = 0; i < passwordLength; i++)
-            {
-                Console.Write((char)random.Next(97, 122));
-            }
-
-            var buffer = new char[passwordLength];
-
-            for (var i = 0; i < passwordLength; i++)
-            {
-                buffer[i] = (char)random.Next(97, 122);
-            }
-
-            var password = new string(buffer);
-            Console.WriteLine("\nPassword: " + password);
+            var arrays = new Arrays();
+            arrays.call();
         }
     }
 }
